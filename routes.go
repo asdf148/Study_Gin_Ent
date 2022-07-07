a5b0cbde638f9b6ab0f896958dd246ad4255fd80
asdf148 <wnduf0405_@naver.com>
2022-07-07 09:11:31 +0900

    todo 라우터 완성

--- routes.go
+++ routes.go
@@ -14,8 +14,12 @@ import (
 
 var (
 	userRepository repository.UserRepository = repository.NewUserRepository()
-	userService service.UserService = service.NewUserService(userRepository)
+	userService    service.UserService       = service.NewUserService(userRepository)
 	userController controller.UserController = controller.NewUserController(userService)
+
+	todoRepository repository.TodoRepository = repository.NewTodoRepository()
+	todoService    service.TodoService       = service.NewTodoService(todoRepository, userRepository)
+	todoController controller.TodoController = controller.NewTodoController(todoService)
 
 	tokenMiddleware middleware.TokenVerification = middleware.NewTokenVerification()
 )
@@ -26,24 +30,12 @@ func initializeRoutes() *gin.Engine {
 	userRoutes := router.Group("user")
 	{
 		userRoutes.POST("/join", func(ctx *gin.Context) {
-			defer func() {
-				if r := recover(); r != nil {
-					error_message := fmt.Sprint(r)
-					errorResponse := dto.NewErrorResponse(error_message)
-					ctx.IndentedJSON(http.StatusBadRequest, errorResponse)
-				}
-			}()
+			defer routerErrorHandler(ctx)
 			ctx.IndentedJSON(http.StatusCreated, userController.Join(ctx))
 		})
 
 		userRoutes.POST("/login", func(ctx *gin.Context) {
-			defer func() {
-				if r := recover(); r != nil {
-					error_message := fmt.Sprint(r)
-					errorResponse := dto.NewErrorResponse(error_message)
-					ctx.IndentedJSON(http.StatusBadRequest, errorResponse)
-				}
-			}()
+			defer routerErrorHandler(ctx)
 			result := userController.Login(ctx)
 			ctx.IndentedJSON(http.StatusCreated, result)
 		})
@@ -51,12 +43,41 @@ func initializeRoutes() *gin.Engine {
 
 	todoRoutes := router.Group("todo")
 	{
+		todoRoutes.GET("/", func(ctx *gin.Context) {
+			defer routerErrorHandler(ctx)
+			ctx.IndentedJSON(http.StatusOK, todoController.FindAllTodo(ctx))
+		})
+
+		todoRoutes.GET("/detail/:todoId", func(ctx *gin.Context) {
+			defer routerErrorHandler(ctx)
+			ctx.IndentedJSON(http.StatusOK, todoController.FindOneTodo(ctx))
+		})
 
 		todoRoutes.Use(tokenMiddleware.TokenVerify)
 		todoRoutes.POST("/", func(ctx *gin.Context) {
-			ctx.IndentedJSON(http.StatusCreated, nil)
+			defer routerErrorHandler(ctx)
+			ctx.IndentedJSON(http.StatusCreated, todoController.AddTodo(ctx))
+		})
+
+		todoRoutes.PUT("/todoId", func(ctx *gin.Context) {
+			defer routerErrorHandler(ctx)
+			ctx.IndentedJSON(http.StatusOK, todoController.ModifyTodo(ctx))
+		})
+
+		todoRoutes.DELETE("/:todoId", func(ctx *gin.Context) {
+			defer routerErrorHandler(ctx)
+			todoController.DeleteTodo(ctx)
+			ctx.IndentedJSON(http.StatusOK, nil)
 		})
 	}
 
 	return router
+}
+
+func routerErrorHandler(ctx *gin.Context) {
+	if r := recover(); r != nil {
+		error_message := fmt.Sprint(r)
+		errorResponse := dto.NewErrorResponse(error_message)
+		ctx.IndentedJSON(http.StatusBadRequest, errorResponse)
+	}
 }
